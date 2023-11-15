4e928e3b12003a4bbce7cfd5abb1c19f0e8ab1c4
박륜 <parkryun@baglyun-ui-MacBookAir.local>
2023-11-15 17:14:06 +0900

    CREATE: error.js & response.js & response.status.js & temp.controller.js & temp.response.dto.js & temp.route.js & temp.service.js & temp.route.js UPDATE: index.js

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 // const express = require('express')   // common JS
-import express from 'express'          // ES6
+import express from 'express';          // ES6
+import { tempRouter } from './src/routes/temp.route.js';
+import { response } from './config/response.js';
+import { BaseError } from './config/error.js';
+import { status } from "./config/response.status.js";
 
 const app = express()
 const port = 3000
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
+
+app.use('/temp', tempRouter);
+
+app.use((req, res, next) => {
+    const err = new BaseError(status.NOT_FOUND);
+    next(err);
+});
+
+// error handling
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.data.status).send(response(err.data));
+
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
